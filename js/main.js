5003e12b8cebf56ed5af30ab4dfe9cd2c7c5ac4d
Шамиль <internetgggg@mail.ru>
2019-09-23 18:30:57 +0300

    Делает Вывод пинов

--- js/main.js
+++ js/main.js
@@ -2,74 +2,82 @@
 
 var map = document.querySelector('.map');
 map.classList.remove('map--faded');
-var markTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
-var adСanvas = document.querySelector('.map__pins');
+var mapPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
+var mapPins = document.querySelector('.map__pins');
 
-var avatars = ['img/avatars/user01.png', 'img/avatars/user02.png', 'img/avatars/user03.png', 'img/avatars/user04.png', 'img/avatars/user05.png', 'img/avatars/user06.png', 'img/avatars/user07.png', 'img/avatars/user08.png'];
 var titles = ['Дом', 'Квартира', 'Замок', 'Теремок', 'Шалаш', 'Пещера', 'Хата', 'Дворец'];
-var addresses = ['Бабушкина', 'Маздокская', 'Кадырова', 'Нурадилова', 'Трощева', 'Эналова', 'Лермонтава', 'Путина'];
-var prices = [2000, 2500, 1500, 4000, 500, 250, 1200, 10000];
-var types = ['palace', 'bungalo', 'flat', 'house', 'palace', 'flat', 'bungalo', 'house'];
-var roomses = [3, 4, 10, 2, 1, 1, 3, 14];
-var guestses = [2, 5, 15, 2, 1, 0, 3, 20];
-var checkins = ['12:00', '14:00', '13:00', '12:00', '13:00', '14:00', '12:00', '13:00'];
-var checkouts = ['13:00', '12:00', '14:00', '14:00', '14:00', '13:00', '13:00', '12:00'];
-var featureses = [['wifi', 'dishwasher', 'parking', 'washer'], ['parking', 'washer', 'elevator', 'conditioner'], ['wifi', 'dishwasher'], ['washer'], ['elevator', 'conditioner'], ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'], ['wifi', 'dishwasher', 'parking']];
-var descriptions = ['aaaaaaaaaaaaaaaaa', 'sssssssssssssssss', 'ddddddddddddddddddd', 'fffffffffffffffff', 'ggggggggggggggg', 'hhhhhhhhhhhhhh', 'eeeeeeeeeeeeee', 'qqqqqqqqqqqqqqq'];
-var photoses = [['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'], ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg'], ['http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'], ['http://o0.github.io/assets/images/tokyo/hotel1.jpg'], ['http://o0.github.io/assets/images/tokyo/hotel2.jpg'], ['http://o0.github.io/assets/images/tokyo/hotel3.jpg'], ['http://o0.github.io/assets/images/tokyo/hotel2.jpg'], ['http://o0.github.io/assets/images/tokyo/hotel3.jpg']];
-var coordinatesX = [100, 140, 250, 360, 180, 430, 330, 290];
-var coordinatesY = [140, 300, 460, 550, 240, 600, 490, 280];
+var types = ['palace', 'bungalo', 'flat', 'house'];
+var checkins = ['12:00', '13:00', '14:00'];
+var featureses = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var descriptions = ['aaaaaaaaaaaaaaaaa', 'sssssssssssssssss', 'ddddddddddddddddddd'];
+
+// ----Генерирует случайное число---------------------
+var getRandomNumber = function (min, max) {
+  return Math.round(min - 0.5 + Math.random() * (max - min + 1));
+};
+// /////////////////////////////////////////////////////////
 
 // ----Создает Стрктуру данных---------------------
-var generationArryAdObjects = function () {
-  var arrySimilarAds = [];
-  for (var i = 0; i < avatars.length; i++) {
-    var similarAd = {
-      author: {},
-      offer: {},
-      location: {}
-    };
-    similarAd.author.avatar = avatars[i];
-    similarAd.offer.title = titles[i];
-    similarAd.offer.address = addresses[i];
-    similarAd.offer.price = prices[i];
-    similarAd.offer.type = types[i];
-    similarAd.offer.rooms = roomses[i];
-    similarAd.offer.guests = guestses[i];
-    similarAd.offer.checkin = checkins[i];
-    similarAd.offer.checkout = checkouts[i];
-    similarAd.offer.features = featureses[i];
-    similarAd.offer.description = descriptions[i];
-    similarAd.offer.photos = photoses[i];
-    similarAd.location.x = coordinatesX[i];
-    similarAd.location.y = coordinatesY[i];
+var getPins = function () {
+  var pinsList = [];
+  for (var i = 0; i < 8; i++) {
+    var featuresesClone = featureses.sort(function () {
+      return Math.random() - 0.5;
+    }).slice();
+    featuresesClone.splice(0, getRandomNumber(1, featuresesClone.length - 1));
 
-    arrySimilarAds.push(similarAd);
+    var location = {
+      x: getRandomNumber(0, map.offsetWidth),
+      y: getRandomNumber(130, 630)
+    };
+
+    var pins = {
+      author: {
+        avatar: 'img/avatars/user0' + (i + 1) + '.png'
+      },
+      offer: {
+        title: titles[getRandomNumber(0, titles.length - 1)],
+        address: location.x + ' ' + location.y,
+        price: getRandomNumber(1000, 1500),
+        rooms: getRandomNumber(1, 15),
+        guests: getRandomNumber(1, 25),
+        checkin: checkins[getRandomNumber(0, checkins.length - 1)],
+        checkout: checkins[getRandomNumber(0, checkins.length - 1)],
+        description: descriptions[getRandomNumber(0, descriptions.length - 1)],
+        photos: 'http://o0.github.io/assets/images/tokyo/hotel' + (i + 1) + '.jpg',
+        type: types[getRandomNumber(0, types.length - 1)],
+        features: featuresesClone
+      },
+      location: location
+    };
+
+    pinsList.push(pins);
   }
-  return arrySimilarAds;
+  return pinsList;
 };
 // ///////////////////////////////////////////////
 
 // ----------Вставляет данные в шаблон
-var fillMarkTemplateData = function (Object) {
-  var markElement = markTemplate.cloneNode(true);
-  markElement.querySelector('img').src = Object.author.avatar;
-  markElement.querySelector('img').alt = Object.offer.title;
-  markElement.style.left = Object.location.x + 'px';
-  markElement.style.top = Object.location.y + 'px';
+var fillMarkTemplateData = function (pin) {
+  var markElement = mapPinTemplate.cloneNode(true);
+  var img = markElement.querySelector('img');
+  img.src = pin.author.avatar;
+  img.alt = pin.offer.title;
+  markElement.style.left = pin.location.x + 'px';
+  markElement.style.top = pin.location.y + 'px';
   return markElement;
 };
 // ////////////////////////////////////////////
 
 // --------------Вставляет готовый шаблон в разметку
-var outputMarks = function () {
-  var arryObjects = generationArryAdObjects();
+var renderPins = function () {
+  var pins = getPins();
   var fragment = document.createDocumentFragment();
-  for (var i = 0; i < arryObjects.length; i++) {
-    fragment.appendChild(fillMarkTemplateData(arryObjects[i]));
+  for (var i = 0; i < pins.length; i++) {
+    fragment.appendChild(fillMarkTemplateData(pins[i]));
   }
-  adСanvas.appendChild(fragment);
+  mapPins.appendChild(fragment);
 };
 // //////////////////////////////////////////////////////
 
-outputMarks();
+renderPins();
