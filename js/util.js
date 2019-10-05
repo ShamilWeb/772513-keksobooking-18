d52a5fe978797e895d8c81571d184480a2b20674
Шамиль <internetgggg@mail.ru>
2019-10-05 14:01:25 +0300

    Исправляет сиетаксические ошибки

--- js/util.js
+++ js/util.js
@@ -8,15 +8,11 @@
       return Math.round(min - 0.5 + Math.random() * (max - min + 1));
     },
     outputErrors: function (errorMessage) {
-        var errorTemplate = document.querySelector('#error').content.querySelector('.error');
-        var errorMessageTpl = errorTemplate.querySelector('.error__message');
-        var main = document.querySelector('main');
-        errorMessageTpl.textContent = errorMessage;
-        main.prepend(errorTemplate);
-      // var div = document.createElement('div');
-      // div.textContent = errorMessage;
-      // div.style = 'position: absolute; z-index: 10; background-color: red; width: 100%; height:50px; display: flex; justify-content: center; align-items: center;';
-      // document.body.prepend(div);
+      var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+      var errorMessageTpl = errorTemplate.querySelector('.error__message');
+      var main = document.querySelector('main');
+      errorMessageTpl.textContent = errorMessage;
+      main.prepend(errorTemplate);
     },
   };
 
